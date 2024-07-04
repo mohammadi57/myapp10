da1a11c74a803314199862aa7c3a882ae2bcec28
ghasem <mohammadighsem883@gmail.com>
2024-07-04 05:59:00 -0700

    salam

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         //---------
         //lkjfl;kj;lkfj;lkajf;lkj
         //dfssssssssssg
-        //خط پنج  تغییر از طرف گیت 
+        //خط پنج  تغییر از طرف گیت
+        // salam
     }
 }
